577099e4d395dedf361ce6d7ae71d7b1e0edb445
Ginta Kivite <D00121871@student.dkit.ie>
2022-10-19 16:59:48 +0100

    examples ini commit

---
+++ Lib/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace Lib
+{
+    public class Logger
+    {
+        public Logger() => XmlConfigurator.Configure();
+        
+        public ILog logger = LogManager.GetLogger(typeof(object));
+
+        public void LogError(string message, string ex, string stackTrace) => logger.ErrorFormat(message, ex, stackTrace);
+
+        public void LogAudit(string message) => logger.WarnFormat(message);
+    }
+
+}
